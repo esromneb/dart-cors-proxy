0aebee6a6cef6d1906f2c6374c2c5b7541b8f6bd
Ben <bjmorse@gmail.com>
2012-04-28 12:24:18 -0700

    Adding auth

--- corsProxy.dart
+++ corsProxy.dart
@@ -2,28 +2,51 @@
 #import("dart:uri");
 #import("dart:utf");
 
-void main() {
-  
+final String GETURL = "";
+final String AUTH = "";
+
+
+final HOST = "127.0.0.1";
+final PORT = 8080;
+final LOG_REQUESTS = true;
+
+void httpGet()
+{
   HttpClient h = new HttpClient();  
   
-  HttpClientConnection conn = h.getUrl(new Uri.fromString("http://user:pw@localhost:5984/"));
-  conn.onResponse = (HttpClientResponse response) {
-    
-  print(response.contentLength);
-  print(response.statusCode);
-    
-    
-  InputStream i = response.inputStream;
-  i.onData = () {
-    print(codepointsToString(i.read()));
-  };
+  HttpClientConnection conn = h.getUrl(new Uri.fromString(GETURL));
   
-  i.onClosed = () { 
-    print("Closed");
-  };
+  // Add Headers once conneciton is opened 
+  conn.onRequest = (HttpClientRequest request) {
 
+    // Set auth header using base64 encoded password
+    request.headers.add("Authorization", AUTH);
   
-};
+    // When all request data has been written close the stream to indicate the end of the request.
+    request.outputStream.close();
+    
+  };
+  
+  conn.onResponse = (HttpClientResponse response) {
+      
+    //print(response.contentLength);
+    //print(response.statusCode);
+      
+      
+    InputStream i = response.inputStream;
+    i.onData = () {
+      print(codepointsToString(i.read()));
+    };
+    
+    i.onClosed = () { 
+      print("Closed");
+    };
+  };
+}
+
+void main() {
+  
+ httpGet();
 
   
   print("Hello World");
