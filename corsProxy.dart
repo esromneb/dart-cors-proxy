b160714895b4c842cecfd8d0dd5057e8205991ab
Ben <bjmorse@gmail.com>
2012-04-28 13:31:02 -0700

    CORS works\!

--- corsProxy.dart
+++ corsProxy.dart
@@ -44,13 +44,16 @@ void httpGet(HttpResponse inputResponse)
       
       
     InputStream i = response.inputStream;
+    
+    i.pipe(inputResponse.outputStream);
+    /*
     i.onData = () {
       print(codepointsToString(i.read()));
     };
     
     i.onClosed = () { 
       print("Closed");
-    };
+    };*/
   };
   
   
@@ -62,84 +65,28 @@ void requestReceivedHandler(HttpRequest inputRequest, HttpResponse inputResponse
   
   //inputResponse.headers.set(HttpHeaders.CONTENT_TYPE, "text/html; charset=UTF-8");
   
-  HttpClient h = new HttpClient();  
-  
-  HttpClientConnection conn = h.getUrl(new Uri.fromString(GETURL));
-  
-  // Add Headers once conneciton is opened 
-  conn.onRequest = (HttpClientRequest request) {
-
-    // Set auth header using base64 encoded password
-    request.headers.add("Authorization", AUTH);
-  
-    // When all request data has been written close the stream to indicate the end of the request.
-    request.outputStream.close();
-    
-  };
-  
-  conn.onResponse = (HttpClientResponse remoteResponse) {
-      
-    //print(response.contentLength);
-    //print(response.statusCode);
-      
-      
-    InputStream i = remoteResponse.inputStream;
-    
-    i.pipe(inputResponse.outputStream);
-    
-   /* i.onData = () {
-      print(codepointsToString(i.read()));
-    };
-    
-    i.onClosed = () { 
-      print("Closed");
-    };*/
-  };
-  
-  
-  
-  
-  
-  
-  
-  
+  inputResponse.headers.set("Access-Control-Allow-Origin", "*");
+  inputResponse.headers.set("Access-Control-Allow-Methods", "OPTIONS, GET, PUT, POST, DELETE");
+  inputResponse.headers.set("Access-Control-Allow-Headers", "Content-Type, Accept, X-Request-With");
+  inputResponse.headers.set("Access-Control-Max-Age", "86400");
   
   
   if (LOG_REQUESTS) {
     print("Request: ${inputRequest.method} ${inputRequest.uri}");
   }
-
-  String htmlResponse = createHtmlResponse();
+  
+ httpGet(inputResponse);
   
   
 
-  
-  
+ // String htmlResponse = createHtmlResponse();
   //response.outputStream.writeString(htmlResponse);
   //response.outputStream.close();
 }
 
-String createHtmlResponse() {
-  return 
-'''
-<html>
-  <style>
-    body { background-color: teal; }
-    p { background-color: white; border-radius: 8px; border:solid 1px #555; text-align: center; padding: 0.5em; 
-        font-family: "Lucida Grande", Tahoma; font-size: 18px; color: #555; }
-  </style>
-  <body>
-    <br/><br/>
-    <p>Current time: ${new Date.now()}</p>
-  </body>
-</html>
-''';
-}
 
 
 void main() {
-  
-//  httpGet();
 
 HttpServer server = new HttpServer();
   
